a67f1ae134dd4f641002bb7b2dac1abf3dc35c16
Logan Donley <ldonley@redhat.com>
2018-07-13 13:14:54 -0400

    Testing

--- openshift-applier/env-init.sh
+++ openshift-applier/env-init.sh
@@ -1,3 +1,2 @@
-ssh root@host01 "docker pull centos/python-35-centos7:latest"
-ssh root@host01 "docker pull openshiftroadshow/parksmap-katacoda:1.0.0"
-ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
+ssh root@host01 "yum install epel-release -y"
+ssh root@host01 "yum install ansible -y"
