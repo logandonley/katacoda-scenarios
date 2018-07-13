5daa1af70eb7bf52d924ed1255360d8d150bf2eb
Logan Donley <ldonley@redhat.com>
2018-07-13 13:48:39 -0400

    Testing

--- openshift-applier/env-init.sh
+++ openshift-applier/env-init.sh
@@ -1,2 +1,4 @@
 ssh root@host01 "yum install epel-release -y"
-ssh root@host01 "yum install ansible -y"
+# ssh root@host01 "yum install ansible -y"
+ssh root@host01 "yum install python34-pip -y"
+ssh root@host01 "pip3 install ansible==2.5"
