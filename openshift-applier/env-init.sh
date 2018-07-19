4642246a01e6303cfcfae40480c468317f29fe7d
Logan Donley <ldonley@redhat.com>
2018-07-19 15:54:40 -0400

    Testing

--- openshift-applier/env-init.sh
+++ openshift-applier/env-init.sh
@@ -1,3 +1,3 @@
 ssh root@host01 "yum install epel-release -y"
 ssh root@host01 "yum install ansible -y"
-ssh root@host01 "oc create -f https://raw.githubusercontent.com/openshift/openshift-ansible/master/roles/openshift_examples/files/examples/v3.10/quickstart-templates/rails-postgresql.json -n openshift"
+ssh root@host01 "oc create -f https://raw.githubusercontent.com/pcarney8/rh-applier-quickstart/master/templates/app/ruby.yml -n openshift"
---
+++ openshift-applier/step2.md
@@ -0,0 +1,6 @@
+For this tutorial, we are going to use an existing Ruby template. In many cases you may want your own custom template, but the process is the same.
+
+First you'll need to find the name of the template you want to pull in:
+
+``oc get templates -n openshift``{{execute}}
+
